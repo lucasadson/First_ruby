a36d398f69fdd521f47e517871328becc9ab868a
Lucas Adson <lucas.adson15@gmail.com>
2023-06-21 13:48:39 -0300

    Multiple Conditions

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,8 @@
 Nome = "Lucas Adson,"
 Ling = "Ruby =)"
+Nameif = "Lucas"
+Cidade = "Ananindeua"
+Pais2 = "Brasil"
 
 puts "#{Nome} está começando a estudar o famoso #{Ling}"
 
@@ -7,14 +10,26 @@ numeros= ["Abacaxi" , "laranja"]
 
 numeros << "uva"
 
+puts numeros
 
+Pais = {:BR => "Brasil", :US => "Estados Unidos", :ES => "Espanha"}
+
+puts Pais
 
 estoque = 11
-if  estoque < 10
+    if  estoque < 10
 
     puts "Abasteça o estoque"
     else
         puts "Ainda não ha necessidade"
 
-end
+    end
+    
+    if  Nameif == "Lucas" && Cidade =="Ananindeua"
 
+puts  "Ta certo meu rei"
+    end
+
+    if Pais2 == "Brasil" || Pais2 == "Espanha"
+        puts "Aqui é #{Pais2} meu rei"
+    end
